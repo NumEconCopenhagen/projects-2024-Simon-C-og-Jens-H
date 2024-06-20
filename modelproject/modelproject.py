dca3091ac0d4e211e8669ee26ef6a74bde3fd63b
zth948 <zth948@alumni.ku.dk>
2024-06-20 14:17:48 +0200

    her

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import optimize
 import matplotlib.pyplot as plt
 from ipywidgets import interact, FloatSlider
+import time
 
 
 class OLG_model:
@@ -142,26 +143,6 @@ class OLG_model:
         
         return k_t, k_t_plus_1, k_t_plus_1_payg, k_star_payg
 
-    def plot_steady_state_payg_interactive(self, tau):
-        k_t, k_t_plus_1, k_t_plus_1_payg, k_star_payg = self.calculate_steady_state_payg()
-        
-        plt.figure(figsize=(10, 6))
-        plt.plot(k_t, k_t, label='45-degree line', linestyle='--')
-        plt.plot(k_t, k_t_plus_1, label='$k_{t+1}$ without PAYG', color='blue')
-        plt.plot(k_t, k_t_plus_1_payg, label='$k_{t+1}$ with PAYG', color='green')
-        plt.scatter(k_star_payg, k_star_payg, color='red', zorder=5, label=f'Steady State with PAYG = {k_star_payg:.3f}')
-        plt.xlabel('$k_t$')
-        plt.ylabel('$k_{t+1}$')
-        plt.title('Capital Accumulation Growth Path with PAYG')
-        plt.legend()
-        plt.grid(True)
-        plt.ylim(0, 0.25)
-        plt.xlim(0, 0.25)
-        plt.show()
-
-    def plot_interactive(self):
-        interact(self.plot_steady_state_payg_interactive, tau=FloatSlider(min=0.0, max=1, step=0.1, value=0.4))
-
     def plot_steady_state_payg_interactive_n(self, tau, n):
         self.par.tau = tau
         self.par.n = n
@@ -182,7 +163,36 @@ class OLG_model:
         plt.show()
     
     def plot_interactive_tau_and_n(self):
-        interact(self.plot_steady_state_payg_interactive_n, tau=FloatSlider(min=0.0, max=1, step=0.1, value=0.4), n=FloatSlider(min=-0.1, max=0.1, step=0.01, value=0.04))
+        interact(self.plot_steady_state_payg_interactive_n, tau=FloatSlider(min=0.0, max=1, step=0.05, value=0.35), n=FloatSlider(min=-0.1, max=0.1, step=0.01, value=0.04))
+
+    def simulate_capital_accumulation(self, k0, periods, n, delay=False):
+        par = self.par
+        k_t = np.zeros(periods)
+        k_t[0] = k0
+        for t in range(1, periods):
+            k_t[t] = (par.A * (1 - par.alpha) * k_t[t-1] ** par.alpha) / ((1 + n) * (2 + par.rho))
+            if delay:
+                time.sleep(0.1)
+        return k_t
+    
+    def plot_convergence(self, k0, periods, delay=False):
+        if self.k_star is None:
+            self.solve_steady_state()
+
+        k_t_old = self.simulate_capital_accumulation(k0, periods, 0.05, delay)
+        k_t_new = self.simulate_capital_accumulation(k0, periods, 0.10, delay)
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(range(periods), k_t_old, label='$k_t$, old (n = 0.05)')
+        plt.plot(range(periods), k_t_new, label='$k_t$, new (n = 0.10)')
+        plt.axhline(y=k_t_old[-1], color='black', linestyle='--', label='analytical steady state (n = 0.05)')
+        plt.axhline(y=k_t_new[-1], color='black', linestyle='--', label='analytical steady state (n = 0.10)')
+        plt.xlabel('Periods')
+        plt.ylabel('Steady state capital')
+        plt.title('Convergence of capital accumulation')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
 
 
     
